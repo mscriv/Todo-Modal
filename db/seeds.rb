43a680310326cbc00c39141dc48d8429708f973d
Michael Scriven <bigscriv03@gmail.com>
2015-01-20 22:05:44 -0500

    Modal complete

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Todo.create!(title: 'grocery shopping', notes: 'pickles, eggs, red onion')
+Todo.create!(title: 'wash the car')
+Todo.create!(title: 'register kids for school', notes: 'register Jayla for sixth grade')
+Todo.create!(title: 'check engine light', notes: 'The check engine light is on in the Tacoma')
+Todo.create!(title: 'dog groomers', notes: 'Take pinky to the groomers')
+
+
